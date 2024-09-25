5ce40a6d1775e10996fe2b7ac4b93a55399df54d
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-09-25 14:43:28 -0300

    feat: :zap: Show match data on screen

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def get_match_data(competition_id : int, season_id: int):
     matches = sb.matches(competition_id=competition_id, season_id=season_id)
     return matches
 
+@st.cache_data(ttl=60*60*24)
+def get_match_events(match_id: int):
+    match_events = sb.events(match_id=match_id)
+    return match_events
+
 dataframe = get_dataframe()
 
 # Selecionar campeonato
@@ -38,7 +43,17 @@ selected_match = st.sidebar.selectbox(
 )
 
 selected_match = matches[matches["match_date"] == selected_match]
+match_events = get_match_events(selected_match["match_id"].values[0])
+
+match_name = selected_match["home_team"][0] + " x " + selected_match["away_team"][0] + " - " + selected_match["match_date"][0]
+
+
+st.markdown("### Campeonato: " + selected_competition + " - Temporada: " + selected_season)
+st.markdown(f"##### {match_name}")
+st.markdown(f"##### Score {selected_match['home_score'][0]} - {selected_match['away_score'][0]}")
+
+st.write(match_events)
+
+
 
 st.write(selected_match)
-
-    
